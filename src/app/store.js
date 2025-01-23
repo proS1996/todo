83e13d6f8cabf96a87c71059b053cd2cdc393397
Apple <apple@Apples-MacBook-Air.local>
2025-01-23 08:22:46 +0530

    fix : update store file for removal of auth api

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authApi } from '../services/rtk-query/authApi';
 import authReducer from '../features/auth/authSlice';
 import { todoApi } from '../services/rtk-query/todoApi';
 
 const store = configureStore({
   reducer: {
-    [authApi.reducerPath]: authApi.reducer,
     [todoApi.reducerPath]: todoApi.reducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware,todoApi.middleware),
+    getDefaultMiddleware().concat(todoApi.middleware),
 });
 
 export default store;
